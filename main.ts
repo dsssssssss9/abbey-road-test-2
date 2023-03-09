c7d2832951f3e37178be4a66dc1665f77c8eabca
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-03-09 18:17:14 +0000

    comment

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+/**
+ * Beta version of extension to allow audio record / playback on a V2 microbit
+ * 
+ * Press BUTTON A to start recording
+ * 
+ * Press BUTTON B to stop recording
+ * 
+ * Press A+B to replay
+ * 
+ * **********************************************
+ * 
+ * * Max record time approx 3 seconds!!!  *
+ * 
+ * **********************************************
+ */
 input.onButtonPressed(Button.A, function () {
     codalAudio.record()
     basic.showIcon(IconNames.EigthNote)
@@ -9,5 +24,5 @@ input.onButtonPressed(Button.B, function () {
     codalAudio.stop()
     basic.showIcon(IconNames.Yes)
 })
-basic.showIcon(IconNames.Diamond)
+basic.showIcon(IconNames.StickFigure)
 codalAudio.setMicrophoneGain(AudioGainEnum.High)
